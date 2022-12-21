3b8f87f976e09c6be1b297aad2c6a520eaffec39
Filip Tarajko <filiptarajko@gmail.com>
2022-12-21 22:27:10 +0100

    make a constant part of miner visible

--- Assets/Code/Scripts/CameraFovEnforcer.cs
+++ Assets/Code/Scripts/CameraFovEnforcer.cs
@@ -6,13 +6,18 @@ public class CameraFovEnforcer : MonoBehaviour
 {
     [SerializeField] Camera this_camera;
     [SerializeField] float target_fov;
-
+    [SerializeField] GameObject cameraParent;
     public static float calculatedFov;
+
+    [Header("a")]
+    [SerializeField] float a;
+    [SerializeField] float b;
 
     // Update is called once per frame
     void Update()
     {
         pSetFOV(target_fov);
+        SetCameraOffset();
     }
 
     private void pSetFOV(float f)
@@ -22,5 +27,9 @@ public class CameraFovEnforcer : MonoBehaviour
        
     }
 
-
+    private void SetCameraOffset()
+    {
+        float x = a*(0.5f / this_camera.aspect) * 16f - b;
+        this_camera.transform.localPosition = new Vector3(0, -x, 0);
+    }
 }
