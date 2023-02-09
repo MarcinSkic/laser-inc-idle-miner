a285c9b9e44448d5daa70db760d2938f4d5cba6b
Filip Tarajko <filiptarajko@gmail.com>
2023-02-09 12:34:42 +0100

    add basic profit prediction analysis tools

--- Assets/Code/Scripts/MVC/Managers/ResourcesManager.cs
+++ Assets/Code/Scripts/MVC/Managers/ResourcesManager.cs
@@ -9,6 +9,61 @@ public class ResourcesManager : MonoBehaviour
     private ResourcesModel model;
 
     public UnityAction<double> onMoneyChange;
+
+    #region AFK gain test
+
+    void updateLastEarnedMoneyStates()
+    {
+        // for debug
+        // IncreaseMoney(1);
+
+        model.lastEarnedMoneyStates.Add(model.earnedMoney);
+        if (model.lastEarnedMoneyStates.Count > 16)
+        {
+            model.lastEarnedMoneyStates.RemoveAt(0);
+        }
+        model.earnedOver15 = model.earnedMoney - model.lastEarnedMoneyStates[0];
+        model.currentPerSecOver15 = model.earnedOver15 / 15f;
+        model.afkToCurrentProportion15 = model.afkGainPerSec15 / model.currentPerSecOver15;
+        if (model.lastEarnedMoneyStates.Count >= 6)
+        {
+            model.earnedOver10 = model.earnedMoney - model.lastEarnedMoneyStates[5];
+            model.currentPerSecOver10 = model.earnedOver10 / 10f;
+            model.afkToCurrentProportion10 = model.afkGainPerSec10 / model.currentPerSecOver10;
+            if (model.lastEarnedMoneyStates.Count >= 11)
+            {
+                model.earnedOver5 = model.earnedMoney - model.lastEarnedMoneyStates[10];
+                model.currentPerSecOver5 = model.earnedOver5 / 5f;
+                model.afkToCurrentProportion5 = model.afkGainPerSec5/ model.currentPerSecOver5;
+            }
+        }
+        if (model.earnedOver15 > model.maxEarnedOver15)
+        {
+            model.maxEarnedOver15 = model.earnedOver15;
+            model.maxPerSecOver15 = model.maxEarnedOver15 / 5f;
+            model.afkGainPerSec15 = model.maxPerSecOver15 / model.maxToAfkProportion15;
+        }
+        if (model.earnedOver10 > model.maxEarnedOver10)
+        {
+            model.maxEarnedOver10 = model.earnedOver10;
+            model.maxPerSecOver10 = model.maxEarnedOver10 / 5f;
+            model.afkGainPerSec10 = model.maxPerSecOver10 / model.maxToAfkProportion10;
+        }
+        if (model.earnedOver5 > model.maxEarnedOver5)
+        {
+            model.maxEarnedOver5 = model.earnedOver5;
+            model.maxPerSecOver5 = model.maxEarnedOver5 / 5f;
+            model.afkGainPerSec5 = model.maxPerSecOver5 / model.maxToAfkProportion5;
+        }
+    }
+
+    private void Start()
+    {
+        InvokeRepeating("updateLastEarnedMoneyStates", 1f, 1f);
+    }
+
+    #endregion
+
     public double Money
     {
         get
--- Assets/Code/Scripts/MVC/Models/ResourcesModel.cs
+++ Assets/Code/Scripts/MVC/Models/ResourcesModel.cs
@@ -14,6 +14,33 @@ public class ResourcesModel : MonoBehaviour
     [ReadOnly]
     public double offlineMoney = 0;
 
+
+    #region AFK gain test
+    [ReadOnly]
+    public List<double> lastEarnedMoneyStates;
+    [SerializeField] public double earnedOver5;
+    [SerializeField] public double earnedOver10;
+    [SerializeField] public double earnedOver15;
+    [SerializeField] public double maxEarnedOver5;
+    [SerializeField] public double maxEarnedOver10;
+    [SerializeField] public double maxEarnedOver15;
+    [SerializeField] public double maxPerSecOver15;
+    [SerializeField] public double maxPerSecOver10;
+    [SerializeField] public double maxPerSecOver5;
+    [SerializeField] public double afkGainPerSec15;
+    [SerializeField] public double afkGainPerSec10;
+    [SerializeField] public double afkGainPerSec5;
+    [SerializeField] public double maxToAfkProportion15;
+    [SerializeField] public double maxToAfkProportion10;
+    [SerializeField] public double maxToAfkProportion5;
+    [SerializeField] public double currentPerSecOver15;
+    [SerializeField] public double currentPerSecOver10;
+    [SerializeField] public double currentPerSecOver5;
+    [SerializeField] public double afkToCurrentProportion15;
+    [SerializeField] public double afkToCurrentProportion10;
+    [SerializeField] public double afkToCurrentProportion5;
+    #endregion
+
     private void OnValidate()
     {
         manager.onMoneyChange?.Invoke(money);
