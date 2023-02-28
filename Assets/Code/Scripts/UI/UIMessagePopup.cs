e8930d2fcce81bb445b6219e6e03a7d54e89f36a
Marcin Skic <marcinskicoficjal@gmail.com>
2023-02-28 10:41:58 +0100

    Finish message popup. Improve locked Achievments look

--- Assets/Code/Scripts/UI/UIMessagePopup.cs
+++ Assets/Code/Scripts/UI/UIMessagePopup.cs
@@ -12,41 +12,30 @@ public class UIMessagePopup : MonoBehaviour
     [SerializeField] private TMP_Text title;
     [SerializeField] private TMP_Text description;
 
-    #region Singleton
-    public static UIMessagePopup Instance;
-    private void Awake()
-    {
-        if (Instance != null)
-        {
-            Destroy(gameObject);
-        }
-        else
-        {
-            Instance = this;
-        }
-    }
-    #endregion
-
-    private void Start()
+    public void Init()
     {
         foreach(var btn in closingButtons)
         {
+            btn.Init();
             btn.onClick += DisablePopup;
         }
     }
 
-    public void DisplayMessage(string title, string value, Sprite icon = null, float delay = 5f)
+    public void DisplayMessage(string title, string value, Sprite icon = null, Color? iconColor = null, float delay = 5f)
     {
         gameObject.SetActive(true);
 
         this.title.text = title;
         description.text = value;
+
         if(icon != null)
         {
+            Debug.Log(iconColor);
             this.icon.sprite = icon;
+            this.icon.color = iconColor ?? Color.white;
         } else
         {
-            iconObject.SetActive(false);
+            this.icon.gameObject.SetActive(false);
         }
 
         StopAllCoroutines();
