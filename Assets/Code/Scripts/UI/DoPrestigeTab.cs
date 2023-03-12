ff252aa876723c6fa94ee5f3b6759ea9ab8293de
Marcin Skic <marcinskicoficjal@gmail.com>
2023-03-12 19:14:41 +0100

    Apply new Achievements and Prestige UI

--- Assets/Code/Scripts/UI/DoPrestigeTab.cs
+++ Assets/Code/Scripts/UI/DoPrestigeTab.cs
@@ -6,27 +6,30 @@ using TMPro;
 
 public class DoPrestigeTab : MonoBehaviour
 {
+    [SerializeField] GameController gameController;
     [SerializeField] ResourcesManager resourcesManager;
     [SerializeField] TMP_Text rewardText;
-    [SerializeField] Button resetButton; 
+    [SerializeField] UIButtonController resetButton; 
 
     void UpdateDisplay()
     {
         double currentReward = resourcesManager.PrestigeCurrencyForNextPrestige;
         if (currentReward > 0)
         {
-            resetButton.interactable = true;
+            resetButton.Activate();
             rewardText.text = $"If you prestige now, you will get <color=\"green\">{NumberFormatter.Format(currentReward)}</color> prestige currency.";
         }
         else
         {
-            resetButton.interactable = false;
+            resetButton.Deactivate();
             rewardText.text = "<color=\"orange\">You need to reach a depth of 500m to be able to prestige.</color>";
         }
     }
 
     private void Start()
     {
+        resetButton.Init();
+        resetButton.onClick += gameController.ExecutePrestige;
         UpdateDisplay();
     }
 
